257c9af9adbbec774d5b8d63f4e498d7df24bb30
guenoel <guenooel@hotmail.com>
2022-08-17 13:16:11 +0200

    operations

--- op.c
+++ op.c
@@ -1,6 +1,19 @@
 #include "monty.h"
 
 /**
+ * newnode - function to create newnode
+ * @num: numero
+ *
+ * return: pointer sur newnode
+ *
+
+stack_t newnode()
+{
+
+}
+*/
+
+/**
  * fpush - function to push new node
  * @s: stack to modify
  * @linenum: line number of the instruction
@@ -9,9 +22,21 @@
 
 void fpush(stack_t **s, unsigned int linenum)
 {
-	(void)s;
-	printf("In fpush, p:%d    , linenum: %d\n", p, linenum);
+    stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = p;
+	node->prev = NULL;
+	node->next = *s;
+
+	if((*s) != NULL)
+		(*s)->prev = node;
+
+	*s = node;
 }
+
 /**
  * fpall - function to print all elements of the stack
  * @s: stack to print
@@ -21,6 +46,11 @@ void fpush(stack_t **s, unsigned int linenum)
 
 void fpall(stack_t **s, unsigned int linenum)
 {
-	(void)s;
-	printf("In fpall, p:%d    , linenum: %d\n", p, linenum);
+	int i;
+
+	for (i = 0; *s != NULL; i++)
+	{
+		printf("%d\n", (*s)->n);
+		*s = (*s)->next;
+	}
 }
